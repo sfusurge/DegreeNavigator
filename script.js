af7544d07091a40c8282527ec78c9e423cccee98
Duy Nguyen <ndnguyen@sfu.ca>
2019-11-27 10:54:59 -0800

    Added about page template

--- script.js
+++ script.js
@@ -273,12 +273,3 @@ function update(source) {
     }
 }
 
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}
-  
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
